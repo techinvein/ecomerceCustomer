cfe2196e719c5a5017dde11e72377e6e461ebc69
pc-145 <arghyadeep@matrixnmedia.com>
2020-12-26 21:35:40 +0530

    pkg name changed

--- android/app/src/main/java/com/techinvein/ecommercecustomer/MainActivity.java
+++ android/app/src/main/java/com/techinvein/ecommercecustomer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.fastfooder;
+package com.techinvein.ecommercecustomer;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "FastFooder";
+    return "EcommerceCustomer";
   }
 }
--- ios/EcommerceCustomerTests/EcommerceCustomerTests.m
+++ ios/EcommerceCustomerTests/EcommerceCustomerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FastFooderTests : XCTestCase
+@interface EcommerceCustomerTests : XCTestCase
 
 @end
 
-@implementation FastFooderTests
+@implementation EcommerceCustomerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
